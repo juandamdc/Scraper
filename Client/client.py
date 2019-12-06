5f9c1908429aa81c995f7d9c27a66ea1cc3f0435
juandamdc <juanda.mdc97@gmail.com>
2019-12-06 13:54:14 -0500

    add the ThreadPoolExecutor

--- Client/client.py
+++ Client/client.py
@@ -1,7 +1,8 @@
+from concurrent.futures import ThreadPoolExecutor
 import rpyc
 import os
-from hashlib import sha256
 import time
+from hashlib import sha256
 
 class Client(rpyc.Service):
 
@@ -25,34 +26,43 @@ class Client(rpyc.Service):
             self.urls.append(url)
 
     def connect(self, ip, port):
-        for url in self.urls:
-            with rpyc.connect(ip, port) as cnn:
-                hash_url = int(sha256(url.encode()).hexdigest(), 16)%2**(10)
+        with ThreadPoolExecutor(max_workers=3) as tpe:
+            for url in self.urls:
+                tpe.submit(lambda p: start_download(*p), [url, ip, port])
+            # download(url, ip, port)
 
-                wait = 0
-                while wait != 3:
-                    try:
-                        _, ip_ask, port_ask = cnn.root.find_successor(hash_url)
-                        break
-                    except Exception:
-                        time.sleep(2**wait)
-                        wait  = wait + 1
+def start_download(url, ip, port):
+    with rpyc.connect(ip, port) as cnn:
+        hash_url = int(sha256(url.encode()).hexdigest(), 16)%2**(10)
 
-            with rpyc.connect(ip_ask, port_ask) as cnn:
-                try:
-                    print(f'INFO: {url} as {sha256(url.encode()).hexdigest()}')
-                    print(f'INFO: ask {url} to ({ip_ask}, {port_ask})')
-                    for page_html in cnn.root.download(url):
+        wait = 0
+        while wait != 3:
+            try:
+                _, ip_ask, port_ask = cnn.root.find_successor(hash_url)
+                download(url, ip_ask, port_ask)
+                break
+            except Exception:
+                time.sleep(2**wait)
+                wait  = wait + 1
 
-                        dir = os.path.join('downloads', page_html[0])
-                        if not os.path.isdir(dir):
-                            os.makedirs(dir)
+def download(url, ip, port):
+    with rpyc.connect(ip, port) as cnn:
+        print(f'INFO: {url} as {sha256(url.encode()).hexdigest()}')
+        print(f'INFO: ask {url} to ({ip}, {port})')
 
-                        with open(os.path.join(dir,page_html[1]), 'wb') as f:
-                            f.write(page_html[2])
+        for page_html in cnn.root.download(url):
+            dir = os.path.join('downloads', page_html[0])
 
-                    # print(f'INFO: saved {url}')
-                    print()
+            if not os.path.isdir(dir):
+                os.makedirs(dir)
 
-                except Exception:
-                    pass
+            with open(os.path.join(dir,page_html[1]), 'wb') as f:
+                f.write(page_html[2])
+
+            if page_html==(0,0,0):
+                print(f'INFO: error in url {url}')
+            elif page_html[1] == 'index.html':
+                print(f'INFO: iniciando descarga {url}')
+
+    print(f'INFO: finalizando descarga {url}')
+    print()
